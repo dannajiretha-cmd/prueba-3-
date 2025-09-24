573d6399afd617f8d0ca58f44bd3820387010756
dannajiretha-cmd <dannajiretha@gmail.com>
2025-09-23 19:49:54 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    SuperBit.Servo(SuperBit.enServo.S1, 0)
+})
+input.onButtonPressed(Button.AB, function () {
+    SuperBit.Servo(SuperBit.enServo.S1, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    SuperBit.Servo(SuperBit.enServo.S1, 180)
+})
